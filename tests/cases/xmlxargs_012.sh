486fcc9769a04d7399143b724401eab495455d25
saulius <saulius@30c38a4c-f28b-4a60-a275-ba9b02e28344>
2018-11-25 21:05:18 +0000

    Implementing ARG_MAX testing in a better way, and adding two tests for this function.
    Using injected POSIX.pm module with artificially low ARG_MAX values for
    these tests.
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/xmlsplit/trunk@38 30c38a4c-f28b-4a60-a275-ba9b02e28344

---
+++ tests/cases/xmlxargs_012.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+set -ue
+
+PERL5LIB=tests/lib/xmlxargs_012:${PERL5LIB}
+
+cat \
+    tests/inputs/cml/cube.cml \
+    tests/inputs/cml/trefoil-on-a-lattice.cml \
+    tests/inputs/cml/hopf-link-minimised.cml \
+| ./xmlxargs -t md5sum 2>&1 \
+| sort -r \
+| perl -pe 's/tmp-xmlxargs-\d+/tmp-xmlxargs-99999/g'
---
+++ tests/lib/xmlxargs_011/POSIX.pm
@@ -0,0 +1,24 @@
+
+# A fake POSIX module to inject special ARG_MAX value for testing.
+
+package POSIX;
+
+our @ISA = qw( Exporter );
+our @EXPORT_OK = qw(
+    sysconf
+);
+
+use strict;
+use warnings;
+
+sub _SC_ARG_MAX
+{
+    return 0;
+}
+
+sub sysconf
+{
+    return 49;
+}
+    
+1;
