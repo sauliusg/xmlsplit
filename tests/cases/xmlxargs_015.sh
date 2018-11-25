aa7336acd8f23192e1707fdc1e43feebead00c72
saulius <saulius@30c38a4c-f28b-4a60-a275-ba9b02e28344>
2018-11-25 21:06:04 +0000

    Implementing '--insert' ling option as a synonim for '-I'.
    Adding tests for both '-I' and '--insert' options.
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/xmlsplit/trunk@42 30c38a4c-f28b-4a60-a275-ba9b02e28344

---
+++ tests/cases/xmlxargs_015.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+set -ue
+
+cat \
+    tests/inputs/cml/cube.cml \
+    tests/inputs/cml/trefoil-on-a-lattice.cml \
+    tests/inputs/cml/hopf-link-minimised.cml \
+    | ./xmlxargs \
+          -s .cml \
+          -I XYZ \
+          -D 7 \
+          -p molecule_ \
+          bash -xc 'echo MD5\(XYZ\) = $(md5sum XYZ)' 2>&1 \
+    | sort \
+    | perl -pe 's/tmp-xmlxargs-\d+/tmp-xmlxargs-99999/g'
