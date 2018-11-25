2e950e1a5119046bc83ea2e283052aa04a9186a1
saulius <saulius@30c38a4c-f28b-4a60-a275-ba9b02e28344>
2018-11-25 21:04:44 +0000

    Testing option '-n'.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/xmlsplit/trunk@34 30c38a4c-f28b-4a60-a275-ba9b02e28344

---
+++ tests/cases/xmlxargs_007.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+set -ue
+
+cat \
+    tests/inputs/cml/cube.cml \
+    tests/inputs/cml/trefoil-on-a-lattice.cml \
+    tests/inputs/cml/hopf-link-minimised.cml \
+| ./xmlxargs -n 2 -t md5sum 2>&1 \
+| sort -r \
+| perl -pe 's/tmp-xmlxargs-\d+/tmp-xmlxargs-99999/g'
