64ca2c9d895efdc32745af0846ca7f1dce731996
saulius <saulius@30c38a4c-f28b-4a60-a275-ba9b02e28344>
2018-11-25 21:04:15 +0000

    Correcting test 'xmlxargs_002'.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/xmlsplit/trunk@31 30c38a4c-f28b-4a60-a275-ba9b02e28344

--- tests/cases/xmlxargs_002.sh
+++ tests/cases/xmlxargs_002.sh
@@ -6,4 +6,5 @@ cat \
     tests/inputs/cml/cube.cml \
     tests/inputs/cml/trefoil-on-a-lattice.cml \
     tests/inputs/cml/hopf-link-minimised.cml \
-| ./xmlxargs md5sum
+| ./xmlxargs md5sum \
+| perl -pe 's/tmp-xmlxargs-\d+/tmp-xmlxargs-99999/g'
